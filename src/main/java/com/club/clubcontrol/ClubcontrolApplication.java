cc0a3a3563842154f3cba8c50734251c5f67fce0
Marcelo Cantarella <marcelo.cantarella@maxisistemas.com.ar>
2023-06-30 10:10:29 -0300

    Proyecto vacio

---
+++ src/main/java/com/club/clubcontrol/ClubcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.club.clubcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/club/clubcontrol/ClubcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.clubcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
